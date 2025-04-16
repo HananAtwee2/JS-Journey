9766b754e15542df2b725d061c91051a23b6bc70
Your Name <atwihanan20@gmail.com>
2025-04-16 12:13:14 +0300

    Average created using reduce and its rounded

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,5 +1,6 @@
 function generateReports(students) {
   return students.map(student => {
     const sum =student.scores.reduce((acc,score)=> acc+score,0);
+    const average=Math.round(sum/student.scores.length);
   })
 }
