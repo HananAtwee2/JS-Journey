4955256112e29cafd633ba745679fbed8b76242b
Your Name <atwihanan20@gmail.com>
2025-04-16 12:16:13 +0300

    Grades are added according to average

--- scripts/logic.js
+++ scripts/logic.js
@@ -2,5 +2,13 @@ function generateReports(students) {
   return students.map(student => {
     const sum =student.scores.reduce((acc,score)=> acc+score,0);
     const average=Math.round(sum/student.scores.length);
+
+    let grade;
+    if (average >= 90) grade = "A";
+    else if (average >= 80) grade = "B";
+    else if (average >= 70) grade = "C";
+    else if (average >= 60) grade = "D";
+    else grade = "F";
+
   })
 }
